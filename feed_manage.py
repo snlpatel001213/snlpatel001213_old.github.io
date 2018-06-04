51e5ed7475c701b76546db7ea0d6799b8b992b05
sunil <snlpatel001213@hotmail.com>
2018-06-04 22:17:40 +0530

    sads

--- feed_manage.py
+++ feed_manage.py
@@ -22,7 +22,7 @@ for each_entry_no in range(len(d.entries)):
         feed_published = json.dumps(d.entries[each_entry_no]['published'])
         # print(feed_published)
 
-        FEED_POST_TRMPLATE = '<div class="chapter"><a href="'+feed_link+'"><img src="'+feed_image+'" alt="'+feed_title+'"></a><div class="chapter_inner"><p class="chapter_number"'+feed_published+'</p><a href="'+feed_link+'"><h3 class="chapter_title">'+feed_title+'</h3></a></div></div>'
+        FEED_POST_TRMPLATE = '<div class="chapter"><a href='+feed_link+'><img src='+feed_image+' alt='+feed_title+'></a><div class="chapter_inner"><p class="chapter_number"'+feed_published+'</p><a href='+feed_link+'s><h3 class="chapter_title">'+feed_title+'</h3></a></div></div>'
         card_string = card_string + FEED_POST_TRMPLATE
         
         # print("#################################################")
